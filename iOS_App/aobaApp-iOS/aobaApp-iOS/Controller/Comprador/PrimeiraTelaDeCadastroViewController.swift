1f9efdf8f1f1b3703f50746e535de839b77dd896
CarlosModinez <modinez.carlos@gmail.com>
2020-08-13 10:59:26 -0300

    tela de login finalizada

---
+++ iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/Comprador/PrimeiraTelaDeCadastroViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PrimeiraTelaDeCadastroViewController.swift
+//  aobaApp-iOS
+//
+//  Created by Carlos Modinez on 13/08/20.
+//  Copyright © 2020 M Cavasin. All rights reserved.
+//
+
+import UIKit
+
+class PrimeiraTelaDeCadastroViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
