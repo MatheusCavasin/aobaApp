b38cb5f7007e117ff31f888c49327ee7896fb534
Matheus Cavasin <matheus.scavasin@gmail.com>
2020-08-14 11:00:20 -0300

    Telas cadastro finalizadas

---
+++ iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/Cadastro1VendedorViewController.swift
@@ -0,0 +1,36 @@
+//
+//  Cadastro1ViewController.swift
+//  aobaApp-iOS
+//
+//  Created by M Cavasin on 13/08/20.
+//  Copyright © 2020 M Cavasin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Cadastro1VendedorViewController: UIViewController {
+
+    @IBOutlet weak var ProximoButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Cadastro"
+        ProximoButton.layer.cornerRadius = 5
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
