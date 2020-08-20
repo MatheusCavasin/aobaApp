be35c3303cf1d511664d48c3b6f5a01aac490a81
CarlosModinez <modinez.carlos@gmail.com>
2020-08-20 10:52:53 -0300

    Ajustes na tabBar e tela de perfil adicionada

--- iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/Comprador/PrincipalComprador.swift
+++ iOS_App/aobaApp-iOS/aobaApp-iOS/Controller/Comprador/PrincipalComprador.swift
@@ -26,18 +26,22 @@ class PrincipalComprador: UITabBarController {
         
         let storyboard1 = UIStoryboard(name: "TabPedidosComprador", bundle: nil)
         let storyboard = UIStoryboard(name: "TabHortifrutiComprador", bundle: nil)
+        let storyboard2 = UIStoryboard(name: "TabPerfilComprador", bundle: nil)
         
         let firstBookTableVc = storyboard.instantiateViewController(withIdentifier: "TabHortifrutiComprador")
         let secondBookTableVc = storyboard1.instantiateViewController(withIdentifier: "TabPedidosComprador")
+        let thirdBookTableVc = storyboard2.instantiateViewController(withIdentifier: "TabPerfilComprador")
         
         //configure the view controllers here...
         
-        viewControllers = [firstBookTableVc, secondBookTableVc]
+        viewControllers = [firstBookTableVc, secondBookTableVc, thirdBookTableVc]
         
         tabBar.items?[0].image = UIImage(named: "icone-hort")
         tabBar.items?[1].image = UIImage(named: "icone-pedidos")
+        tabBar.items?[2].image = UIImage(named: "icone-perfil")
         
         tabBar.items?[0].title = "Hortifruti"
         tabBar.items?[1].title = "Pedidos"
+        tabBar.items?[2].title = "Perfil"
     }
 }
